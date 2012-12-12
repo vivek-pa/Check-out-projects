ca21b146cdd93043b6cb067ba07517d3bdee580b
vivek-pa <vivek.parthiban@photoninfotech.net>
2012-12-12 21:04:02 +0530

    new files for drupal7 and wordpress projects

---
+++ V-tech-phpdru7/test/unit/src/test/php/phresco/tests/NodeModules.php
@@ -0,0 +1,27 @@
+<?php
+/*	
+
+Author by {phresco} QA Automation Team	
+
+*/
+
+require 'tests/Node.php';
+
+    class NodeModules extends PHPUnit_Framework_TestSuite {
+    protected $backupGlobals = FALSE;
+    
+	protected function setUp() {
+	}
+
+	public static function suite()
+	{
+		$testSuite = new NodeModules('NodeModules');
+		$testSuite->addTest(new Node("testLastChanged"));
+		$testSuite->addTest(new Node("testNodeTitle"));
+		return $testSuite;
+	}
+	protected function tearDown() {
+	}
+
+} 
+?>
